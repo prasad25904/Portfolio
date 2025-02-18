dfbe1f306f84b050d4a8f9d04aa732ec54291859
Prasad Ingle <prasadingle25904@gmail.com>
2025-02-18 14:53:15 +0530

    Footer improved link added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Footer.css';
-import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
   return (
@@ -18,17 +18,17 @@ const Footer = () => {
         </div>
         
         <div className="footer-socials">
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-            <FaFacebook />
+          <a href="https://www.linkedin.com/in/prasad-ingle-639731261/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin />
+          </a>
+          <a href="https://github.com/prasad25904" target="_blank" rel="noopener noreferrer">
+            <FaGithub />
           </a>
           <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
             <FaTwitter />
           </a>
           <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
             <FaInstagram />
-          </a>
-          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-            <FaLinkedin />
           </a>
         </div>
       </div>
